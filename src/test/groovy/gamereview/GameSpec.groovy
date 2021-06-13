f77bcf8fc538659bf51ecb198e1a9d16d2bcfc63
rmsolera <rmsolera@mail.com>
2021-06-13 14:36:12 +0200

    Changed some params in application.yml

--- src/test/groovy/gamereview/GameSpec.groovy
+++ src/test/groovy/gamereview/GameSpec.groovy
@@ -15,4 +15,16 @@ class GameSpec extends Specification implements DomainUnitTest<Game> {
         expect:"fix me"
             true == false
     }
+
+    // void "Test that Game has properties name, description and price"() {  
+    // when: "A game is created with the given name, description and price"  
+    //     String name = 'GTA'
+    //     String description = 'This is the best car game ever developed'  
+    //     String price  = '34.9'  
+    //     Game game = new Game(name:name, description:description, price:price)  
+    // then: "The details are set to the Game"  
+    //     game.name == name  
+    //     game.description == description
+    //     game.price == price 
+//}  
 }
